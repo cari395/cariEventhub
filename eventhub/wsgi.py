f6e52a287af30cd3a8070eb8fa81ebec7b2fa49f
cari395 <carinimatu11@live.com.ar>
2025-06-12 15:49:29 -0300

    Cambios al cd

--- eventhub/wsgi.py
+++ eventhub/wsgi.py
@@ -8,9 +8,20 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 """
 
 import os
-
+import django
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eventhub.settings")
 
+# Inicializar Django
+django.setup()
+
+# Ejecutar migraciones automáticamente
+from django.core.management import call_command
+try:
+    call_command('migrate', interactive=False)
+except Exception as e:
+    print(f"Error ejecutando migrate: {e}")
+
+# Obtener la aplicación WSGI
 application = get_wsgi_application()
